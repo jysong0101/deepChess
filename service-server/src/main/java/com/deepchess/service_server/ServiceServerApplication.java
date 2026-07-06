b57f72c8d2cf696c68c262f4df46fb59b2d48873
Juyeop Song <118370505+jysong0101@users.noreply.github.com>
2026-07-06 05:07:54 +0000

    generate spring

---
+++ service-server/src/main/java/com/deepchess/service_server/ServiceServerApplication.java
@@ -0,0 +1,13 @@
+package com.deepchess.service_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceServerApplication.class, args);
+	}
+
+}
---
+++ service-server/src/test/java/com/deepchess/service_server/ServiceServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deepchess.service_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
